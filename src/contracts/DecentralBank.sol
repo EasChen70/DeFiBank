1824431d87e9569146bfebba3210849b84d85322
EasChen70 <eason_chen@uri.edu>
2024-09-17 22:02:02 -0400

    deposit for staking function

--- src/contracts/DecentralBank.sol
+++ src/contracts/DecentralBank.sol
@@ -9,9 +9,32 @@ contract DecentralBank{
     Tether public tether;
     Reward public reward;
 
+    address[] public stakers;
+
+    mapping(address => uint) public stakingBalance;
+    mapping(address => bool) public hasStaked;
+    mapping(address => bool) public isStaked;
     constructor(Reward _reward, Tether _tether){
         reward = _reward;
         tether = _tether;
     }
 
+    // Staking function
+    function depositTokens(uint _amount) public{
+        //prereq for staking
+        require(_amount > 0, 'Invalid amount');
+
+        //Transfer tether to this contract address for staking
+        tether.transferFrom(msg.sender, address(this), _amount);
+
+        //Update staking balance
+        stakingBalance[msg.sender] += _amount;
+
+        if(!hasStaked[msg.sender]){
+            stakers.push(msg.sender);   
+        }
+
+        isStaked[msg.sender] = true;
+        hasStaked[msg.sender] = true;
+    }
 }
